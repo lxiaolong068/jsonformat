e3d012dadb63c63a2fceefbe46410040548a51a9
李剑飞 <152262674+lxiaolong068@users.noreply.github.com>
2025-03-07 00:06:59 +0700

    add sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,47 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://jsoncrack.com",
+  siteUrl: "https://www.jsonformat.help",
   exclude: ["/widget"],
   autoLastmod: false,
+  generateRobotsTxt: true,
+  trailingSlash: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  alternateRefs: [
+    {
+      href: 'https://www.jsonformat.help',
+      hreflang: 'en'
+    },
+    {
+      href: 'https://www.jsonformat.help/zh',
+      hreflang: 'zh'
+    },
+    {
+      href: 'https://www.jsonformat.help/ja',
+      hreflang: 'ja'
+    },
+    {
+      href: 'https://www.jsonformat.help/ko',
+      hreflang: 'ko'
+    },
+    {
+      href: 'https://www.jsonformat.help/de',
+      hreflang: 'de'
+    }
+  ],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    additionalSitemaps: [
+      'https://www.jsonformat.help/sitemap-en.xml',
+      'https://www.jsonformat.help/sitemap-zh.xml',
+      'https://www.jsonformat.help/sitemap-ja.xml',
+      'https://www.jsonformat.help/sitemap-ko.xml',
+      'https://www.jsonformat.help/sitemap-de.xml'
+    ],
+  },
 };
